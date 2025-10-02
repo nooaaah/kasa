c00c6f68ed6c5f94d78375753a9f553dbdcac87a
noah bernardi <noah.bernardi12@gmail.com>
2025-10-02 15:48:11 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+import './index.css'
+import App from './App.jsx'
+import APropos from './APropos.jsx';
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/a-propos" element={<APropos />} />
+    </Routes>
+    
+  </BrowserRouter>,
+)
